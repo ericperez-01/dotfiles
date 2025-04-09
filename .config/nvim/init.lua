f0d7c3d729881603f4618d4eb2c5e7b493adc882
Eric Perez <ericperez01@gmail.com>
2025-04-09 06:09:58 -0700

    Updated nvim to add  debugging

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep) end},
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
     config = function() require("nvim-treesitter.configs").setup {
-      ensure_installed = {"lua", "javascript", "python", "html", "css"},
+      ensure_installed = {"lua", "javascript", "python", "html", "css", "rust"},
       highlight = {enable = true}, indent = {enable = true}} end},
   {"yetone/avante.nvim", event = "VeryLazy", build = "make",
     dependencies = {"nvim-treesitter/nvim-treesitter", "stevearc/dressing.nvim",
@@ -58,10 +58,28 @@ require("lazy").setup({
   {"windwp/nvim-autopairs", event = "InsertEnter", config = true},
   {"numToStr/Comment.nvim", config = true},
   {"lewis6991/gitsigns.nvim", config = true},
-  {"williamboman/mason.nvim", config = function() require("mason").setup() end},
+  {"williamboman/mason.nvim", 
+    config = function() 
+      require("mason").setup() 
+    end
+  },
   {"williamboman/mason-lspconfig.nvim",
-    config = function() require("mason-lspconfig").setup {
-      ensure_installed = {"lua_ls", "pyright", "ts_ls", "rust_analyzer"}} end},
+    config = function() 
+      require("mason-lspconfig").setup {
+        ensure_installed = {"lua_ls", "pyright", "ts_ls", "rust_analyzer"}
+      } 
+    end
+  },
+  {"jay-babu/mason-nvim-dap.nvim",
+    dependencies = {"williamboman/mason.nvim"},
+    config = function()
+      require("mason-nvim-dap").setup({
+        ensure_installed = {"codelldb"},
+        automatic_installation = true,
+        handlers = {},
+      })
+    end
+  },
   {"neovim/nvim-lspconfig",
     dependencies = {"hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip"},
@@ -101,6 +119,148 @@ require("lazy").setup({
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
+  },
+  -- Added debugging plugins
+  {"mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+
+      -- Keymappings for debugging
+      vim.keymap.set('n', '<F5>', function() dap.continue() end)
+      vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+      vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+      vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+      vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end)
+      vim.keymap.set('n', '<leader>dB', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+      vim.keymap.set('n', '<leader>dr', function() dap.repl.open() end)
+      vim.keymap.set('n', '<leader>dl', function() dap.run_last() end)
+
+      -- Configure Rust debugging with codelldb
+      dap.adapters.codelldb = {
+        type = 'server',
+        port = "${port}",
+        executable = {
+          -- This path will be filled by mason-nvim-dap
+          command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+          args = {"--port", "${port}"},
+        }
+      }
+
+      dap.configurations.rust = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            -- Get the package name from Cargo.toml
+            local cargo_toml = vim.fn.fnamemodify(vim.fn.getcwd(), ":p") .. "/Cargo.toml"
+            if vim.fn.filereadable(cargo_toml) == 1 then
+              local content = vim.fn.readfile(cargo_toml)
+              for _, line in ipairs(content) do
+                if line:match("^name%s*=%s*\"(.+)\"") then
+                  local package_name = line:match("^name%s*=%s*\"(.-)\"")
+                  local target_dir = vim.fn.getcwd() .. "/target/debug/" .. package_name
+                  if vim.fn.executable(target_dir) == 1 then
+                    return target_dir
+                  end
+                end
+              end
+            end
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = false,
+          args = {},
+        },
+        {
+          name = "Attach to process",
+          type = "codelldb",
+          request = "attach",
+          pid = require('dap.utils').pick_process,
+          args = {},
+        },
+      }
+    end
+  },
+  {"rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+      local dapui = require("dapui")
+      dapui.setup({
+        icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+        mappings = {
+          -- Use a table to apply multiple mappings
+          expand = { "<CR>", "<2-LeftMouse>" },
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+          toggle = "t",
+        },
+        layouts = {
+          {
+            elements = {
+              -- Elements can be strings or table with id and size keys.
+              { id = "scopes", size = 0.25 },
+              "breakpoints",
+              "stacks",
+              "watches",
+            },
+            size = 40, -- 40 columns
+            position = "left",
+          },
+          {
+            elements = {
+              "repl",
+              "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+          },
+        },
+        controls = {
+          -- Requires Neovim nightly (or 0.8 when released)
+          enabled = true,
+          -- Display controls in this element
+          element = "repl",
+          icons = {
+            pause = "⏸",
+            play = "▶",
+            step_into = "⏎",
+            step_over = "⏭",
+            step_out = "⏮",
+            step_back = "b",
+            run_last = "▶▶",
+            terminate = "⏹",
+          },
+        },
+        floating = {
+          max_height = nil, -- These can be integers or a float between 0 and 1.
+          max_width = nil, -- Floats will be treated as percentage of your screen.
+          border = "single", -- Border style. Can be "single", "double" or "rounded"
+          mappings = {
+            close = { "q", "<Esc>" },
+          },
+        },
+      })
+      
+      -- Automatically open and close dapui when debugging starts/ends
+      local dap = require("dap")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+      
+      -- Keymappings for dapui
+      vim.keymap.set('n', '<leader>du', function() dapui.toggle() end)
+      vim.keymap.set('n', '<leader>de', function() dapui.eval() end)
+      vim.keymap.set('v', '<leader>de', function() dapui.eval() end)
+    end
   },
 })
 
