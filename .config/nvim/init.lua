95a1e5f8b214a26a3bf5546ae68236b64cf201a1
Eric Perez <ericperez01@gmail.com>
2025-04-09 09:10:30 -0700

    Updated nvim to add more rust debugging

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,16 +25,66 @@ if not vim.loop.fs_stat(lp) then
 end
 vim.opt.rtp:prepend(lp)
 
+-- Set up rustaceanvim configuration before loading plugins
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {
+    -- Automatically set inlay hints
+    inlay_hints = {
+      auto = true,
+    },
+    -- Configure test executor
+    test_executor = 'background', -- Can also be set to 'neotest'
+  },
+  -- DAP configuration
+  dap = {
+    adapter = {
+      type = "executable",
+      command = "codelldb",
+      name = "rt_lldb",
+    },
+  },
+  -- Server configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- Enable completion triggered by <c-x><c-o>
+      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+      
+      -- Buffer local mappings
+      local opts = { noremap=true, silent=true, buffer=bufnr }
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    end,
+    settings = {
+      -- rust-analyzer settings
+      ["rust-analyzer"] = {
+        -- Enable experimental features
+        checkOnSave = {
+          command = "clippy",
+        },
+        inlayHints = {
+          lifetimeElisionHints = {
+            enable = "always",
+          },
+        },
+      },
+    },
+  },
+}
+
 require("lazy").setup({
-{"nvim-neo-tree/neo-tree.nvim", dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim"},
-  config = function() require("neo-tree").setup({filesystem = {commands = {avante_add_files = function(state)
-    local node, path = state.tree:get_node(), state.tree:get_node():get_id()
-    local sidebar, open = require('avante').get(), require('avante').get():is_open()
-    if not open then require('avante.api').ask() sidebar = require('avante').get() end
-    sidebar.file_selector:add_selected_file(require('avante.utils').relative_path(path))
-    if not open then sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]') end
-  end}, window = {mappings = {["oa"] = "avante_add_files"}}}})
-  vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {silent = true}) end},
+  {"nvim-neo-tree/neo-tree.nvim", dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim"},
+    config = function() require("neo-tree").setup({filesystem = {commands = {avante_add_files = function(state)
+      local node, path = state.tree:get_node(), state.tree:get_node():get_id()
+      local sidebar, open = require('avante').get(), require('avante').get():is_open()
+      if not open then require('avante.api').ask() sidebar = require('avante').get() end
+      sidebar.file_selector:add_selected_file(require('avante.utils').relative_path(path))
+      if not open then sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]') end
+    end}, window = {mappings = {["oa"] = "avante_add_files"}}}})
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {silent = true}) end},
   {"nvim-telescope/telescope.nvim", dependencies = {"nvim-lua/plenary.nvim"},
     config = function() require("telescope").setup()
       vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
@@ -66,7 +116,8 @@ require("lazy").setup({
   {"williamboman/mason-lspconfig.nvim",
     config = function() 
       require("mason-lspconfig").setup {
-        ensure_installed = {"lua_ls", "pyright", "ts_ls", "rust_analyzer"}
+        -- Remove rust_analyzer as it will be handled by rustaceanvim
+        ensure_installed = {"lua_ls", "pyright", "ts_ls"}
       } 
     end
   },
@@ -85,13 +136,9 @@ require("lazy").setup({
       "hrsh7th/cmp-path", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip"},
     config = function()
       local caps = require("cmp_nvim_lsp").default_capabilities()
-      for _, lsp in ipairs({"lua_ls", "pyright", "ts_ls", "rust_analyzer"}) do
+      for _, lsp in ipairs({"lua_ls", "pyright", "ts_ls"}) do
         require("lspconfig")[lsp].setup {capabilities = caps} end
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-      vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format {async = true} end)
+      
       local cmp = require("cmp") local ls = require("luasnip")
       cmp.setup {snippet = {expand = function(args) ls.lsp_expand(args.body) end},
         mapping = cmp.mapping.preset.insert {
@@ -108,7 +155,6 @@ require("lazy").setup({
         sources = cmp.config.sources({{name = "nvim_lsp"}, {name = "luasnip"},
           {name = "buffer"}, {name = "path"}})}
     end},
-  -- Added flash.nvim plugin
   {"folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
@@ -120,66 +166,19 @@ require("lazy").setup({
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
-  -- Added debugging plugins
-  {"mfussenegger/nvim-dap",
+  -- Add rustaceanvim for Rust development and test debugging
+  {"mrcjkb/rustaceanvim",
+    version = "^5", -- Use version 5 or higher
+    ft = "rust", -- Only load when editing Rust files
+    dependencies = {"mfussenegger/nvim-dap"},
     config = function()
-      local dap = require("dap")
-
-      -- Keymappings for debugging
-      vim.keymap.set('n', '<F5>', function() dap.continue() end)
-      vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-      vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-      vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-      vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end)
-      vim.keymap.set('n', '<leader>dB', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
-      vim.keymap.set('n', '<leader>dr', function() dap.repl.open() end)
-      vim.keymap.set('n', '<leader>dl', function() dap.run_last() end)
-
-      -- Configure Rust debugging with codelldb
-      dap.adapters.codelldb = {
-        type = 'server',
-        port = "${port}",
-        executable = {
-          -- This path will be filled by mason-nvim-dap
-          command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
-          args = {"--port", "${port}"},
-        }
-      }
-
-      dap.configurations.rust = {
-        {
-          name = "Launch file",
-          type = "codelldb",
-          request = "launch",
-          program = function()
-            -- Get the package name from Cargo.toml
-            local cargo_toml = vim.fn.fnamemodify(vim.fn.getcwd(), ":p") .. "/Cargo.toml"
-            if vim.fn.filereadable(cargo_toml) == 1 then
-              local content = vim.fn.readfile(cargo_toml)
-              for _, line in ipairs(content) do
-                if line:match("^name%s*=%s*\"(.+)\"") then
-                  local package_name = line:match("^name%s*=%s*\"(.-)\"")
-                  local target_dir = vim.fn.getcwd() .. "/target/debug/" .. package_name
-                  if vim.fn.executable(target_dir) == 1 then
-                    return target_dir
-                  end
-                end
-              end
-            end
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
-          end,
-          cwd = '${workspaceFolder}',
-          stopOnEntry = false,
-          args = {},
-        },
-        {
-          name = "Attach to process",
-          type = "codelldb",
-          request = "attach",
-          pid = require('dap.utils').pick_process,
-          args = {},
-        },
-      }
+      -- Key mappings for Rust debugging and testing
+      vim.keymap.set("n", "<leader>rt", function() vim.cmd.RustLsp("testables") end, {desc = "Rust Testables", silent = true})
+      vim.keymap.set("n", "<leader>rr", function() vim.cmd.RustLsp("runnables") end, {desc = "Rust Runnables", silent = true})
+      vim.keymap.set("n", "<leader>rd", function() vim.cmd.RustLsp("debuggables") end, {desc = "Rust Debuggables", silent = true})
+      vim.keymap.set("n", "<leader>rm", function() vim.cmd.RustLsp("expandMacro") end, {desc = "Expand Rust Macro", silent = true})
+      vim.keymap.set("n", "<leader>rc", function() vim.cmd.RustLsp("openCargo") end, {desc = "Open Cargo.toml", silent = true})
+      vim.keymap.set("n", "<leader>rp", function() vim.cmd.RustLsp("parentModule") end, {desc = "Go to Parent Module", silent = true})
     end
   },
   {"rcarriga/nvim-dap-ui",
@@ -189,7 +188,6 @@ require("lazy").setup({
       dapui.setup({
         icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
         mappings = {
-          -- Use a table to apply multiple mappings
           expand = { "<CR>", "<2-LeftMouse>" },
           open = "o",
           remove = "d",
@@ -200,13 +198,12 @@ require("lazy").setup({
         layouts = {
           {
             elements = {
-              -- Elements can be strings or table with id and size keys.
               { id = "scopes", size = 0.25 },
               "breakpoints",
               "stacks",
               "watches",
             },
-            size = 40, -- 40 columns
+            size = 40,
             position = "left",
           },
           {
@@ -214,14 +211,12 @@ require("lazy").setup({
               "repl",
               "console",
             },
-            size = 0.25, -- 25% of total lines
+            size = 0.25,
             position = "bottom",
           },
         },
         controls = {
-          -- Requires Neovim nightly (or 0.8 when released)
           enabled = true,
-          -- Display controls in this element
           element = "repl",
           icons = {
             pause = "⏸",
@@ -235,15 +230,15 @@ require("lazy").setup({
           },
         },
         floating = {
-          max_height = nil, -- These can be integers or a float between 0 and 1.
-          max_width = nil, -- Floats will be treated as percentage of your screen.
-          border = "single", -- Border style. Can be "single", "double" or "rounded"
+          max_height = nil,
+          max_width = nil,
+          border = "single",
           mappings = {
             close = { "q", "<Esc>" },
           },
         },
       })
-      
+
       -- Automatically open and close dapui when debugging starts/ends
       local dap = require("dap")
       dap.listeners.after.event_initialized["dapui_config"] = function()
@@ -255,11 +250,27 @@ require("lazy").setup({
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
-      
+
       -- Keymappings for dapui
       vim.keymap.set('n', '<leader>du', function() dapui.toggle() end)
       vim.keymap.set('n', '<leader>de', function() dapui.eval() end)
       vim.keymap.set('v', '<leader>de', function() dapui.eval() end)
+    end
+  },
+  -- Standard DAP keybindings
+  {"mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+
+      -- Keymappings for debugging
+      vim.keymap.set('n', '<F5>', function() dap.continue() end)
+      vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+      vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+      vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+      vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end)
+      vim.keymap.set('n', '<leader>dB', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+      vim.keymap.set('n', '<leader>dr', function() dap.repl.open() end)
+      vim.keymap.set('n', '<leader>dl', function() dap.run_last() end)
     end
   },
 })
@@ -284,20 +295,20 @@ vim.keymap.set("n", "<M-j>", "<C-w>j", {silent = true})
 vim.keymap.set("n", "<M-k>", "<C-w>k", {silent = true})
 
 -- Window management keybindings
-vim.keymap.set("n", "<leader>|", ":vsplit<CR>", {silent = true})                          -- Vertical split (like tmux)
-vim.keymap.set("n", "<leader>-", ":split<CR>", {silent = true})                           -- Horizontal split (like tmux)
-vim.keymap.set("n", "<leader>t", ":tabnew<CR>", {silent = true})                          -- New tab
-vim.keymap.set("n", "<leader>c", ":close<CR>", {silent = true})                           -- Close current split/tab
-vim.keymap.set("n", "<leader>=", "5<C-w>+", {silent = true})                              -- Increase split height
-vim.keymap.set("n", "<leader>_", "5<C-w>-", {silent = true})                              -- Decrease split height
-vim.keymap.set("n", "<leader>>", "5<C-w>>", {silent = true})                              -- Increase split width
-vim.keymap.set("n", "<leader><", "5<C-w><", {silent = true})                              -- Decrease split width
-vim.keymap.set("n", "<leader>n", ":tabnext<CR>", {silent = true})                         -- Next tab
-vim.keymap.set("n", "<leader>p", ":tabprevious<CR>", {silent = true})                     -- Previous tab
+vim.keymap.set("n", "<leader>|", ":vsplit<CR>", {silent = true})
+vim.keymap.set("n", "<leader>-", ":split<CR>", {silent = true})
+vim.keymap.set("n", "<leader>t", ":tabnew<CR>", {silent = true})
+vim.keymap.set("n", "<leader>c", ":close<CR>", {silent = true})
+vim.keymap.set("n", "<leader>=", "5<C-w>+", {silent = true})
+vim.keymap.set("n", "<leader>_", "5<C-w>-", {silent = true})
+vim.keymap.set("n", "<leader>>", "5<C-w>>", {silent = true})
+vim.keymap.set("n", "<leader><", "5<C-w><", {silent = true})
+vim.keymap.set("n", "<leader>n", ":tabnext<CR>", {silent = true})
+vim.keymap.set("n", "<leader>p", ":tabprevious<CR>", {silent = true})
 
 -- Buffer navigation with Ctrl
-vim.keymap.set("n", "<C-h>", ":bprevious<CR>", { silent = true })  -- Previous buffer
-vim.keymap.set("n", "<C-l>", ":bnext<CR>", { silent = true })      -- Next buffer
+vim.keymap.set("n", "<C-h>", ":bprevious<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":bnext<CR>", { silent = true })
 
 -- Focus on Neotree
 vim.keymap.set("n", "<leader>E", ":Neotree reveal<CR>", {silent = true})
